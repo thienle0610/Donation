f3de34ad1d61587dfeea99dfbec82a91a2b324c9
thienle0610 <67705332+thienle0610@users.noreply.github.com>
2020-11-21 19:40:02 -0800

    Add files to Github

---
+++ Donation/index.js
@@ -0,0 +1,34 @@
+function store(){
+
+    var fname = document.getElementById('fn');
+    var lname = document.getElementById('ln');
+    var phoneno= document.getElementById('pn');
+    var email = document.getElementById('email');
+
+    if(fname.value.length == 0){
+        alert('Please fill in first name');
+
+    }else if(lname.value.length == 0){
+        alert('Please fill in last name');
+
+    }else if(phoneno.value.length == 0){
+        alert('Please fill in phone number');
+
+    }else if(email.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(fname.value.length == 0 && lname.value.length == 0 && phoneno.value.length == 0 && email.value.length == 0){
+        alert('Please fill in first name, last name, phone number and email');
+
+    }else{
+        localStorage.setItem('fname', fname.value);
+        localStorage.setItem('lname', lname.value);
+        localStorage.setItem('phoneno', phoneno.value);
+        localStorage.setItem('email', email.value);
+        alert('Your donation is successful! Thank you!');
+    }
+}
+
+
+
+
